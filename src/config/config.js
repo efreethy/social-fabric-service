2897e786006e4f7c5d4ca1496c76c8499c9cabe4
efreethy <efreethy@gmail.com>
2019-11-24 08:29:05 -0800

    dont recreate /build on prod deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
+console.log('\n\n HYYYYY process.env.INSTANCE_CONNECTION_NAME', process.env.INSTANCE_CONNECTION_NAME)
+
 module.exports = {
   development: {
     database: process.env.DB_NAME || 'postgres',
@@ -21,7 +23,7 @@ module.exports = {
     database: process.env.DB_NAME || 'postgres',
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'p@ssw0rd',
-    host: '/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}',
+    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
     authentication_token_secret: (process.env.AUTH_TOKEN_SECRET || 'dev-secret'),
     dialect: 'postgres',
     logging: true
